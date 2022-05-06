de4282cb737ac4373722455b85787044736b098d
cegj <cegasparjr@gmail.com>
2022-05-06 16:24:39 -0300

    Set name on document title

--- assets/script/page.js
+++ assets/script/page.js
@@ -10,7 +10,29 @@ export default class Page{
         return page;    
     }
 
+    setPtPageName(pageName){
+        switch(pageName){
+            case 'board':
+                this.ptPageName = 'Painel';
+                break;
+            case 'statement':
+                this.ptPageName = 'Extrato';
+                break;        
+            case 'budget':
+                this.ptPageName = 'Orçamento';
+                break;
+            case 'category':
+                this.ptPageName = 'Categorias';
+                break;
+            case 'account':
+                this.ptPageName = 'Contas';
+                break;
+        }
+        return this.ptPageName;
+    }
+
     async load(){
         this.target.innerHTML = await this.fetchPage();
+        document.title = "Contta / " + this.setPtPageName(this.pageName);
     }
 }
--- partes-template/header.php
+++ partes-template/header.php
@@ -4,7 +4,7 @@
     <img class="logo" height="70px" src="/img/contta.png" alt="Contta" />
     <nav aria-label="menu-principal">
       <ul class="menu-principal">
-        <li><a class="botao-menu-principal" href="?p=index">Painel</a></li>
+        <li><a class="botao-menu-principal" href="?p=board">Painel</a></li>
         <li><a class="botao-menu-principal" href="?p=statement">Extrato</a></li>
         <li><a class="botao-menu-principal" href="?p=budget">Orçamento</a></li>
         <li><a class="botao-menu-principal" href="?p=category">Categorias</a></li>
