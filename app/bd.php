f675b9bb3c44d1b23103ca3aa01d701265500739
cegj <cegasparjr@gmail.com>
2022-06-18 00:48:58 -0300

    Use env variables to db info

--- app/bd.php
+++ app/bd.php
@@ -2,9 +2,9 @@
 
 //Conexão com o banco de dados
 
-$dbServer = '127.0.0.1';
-$dbUser = 'root';
-$dbPassword = '';
-$dbName = 'bdcontta';
+$dbServer = getenv("DB_SERVER") ? getenv("DB_SERVER") : '127.0.0.1';
+$dbUser = getenv("DB_USER") ? getenv("DB_USER") : 'root';
+$dbPassword = getenv("DB_PASSWORD") ? getenv("DB_PASSWORD") : '';
+$dbName = getenv("DB_NAME") ? getenv("DB_NAME") : 'bdcontta';
 
 $bdConexao = mysqli_connect($dbServer, $dbUser, $dbPassword, $dbName);
