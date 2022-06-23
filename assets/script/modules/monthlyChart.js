7fa1fe19a2938db5cffbf5c589a1f402db785baf
cegj <cegasparjr@gmail.com>
2022-06-23 19:46:03 -0300

    Set categories monthly chart

--- assets/script/modules/monthlyChart.js
+++ assets/script/modules/monthlyChart.js
@@ -4,17 +4,62 @@ export default class MonthlyChart {
     constructor(targetSelector, accountSelectSelector, catSelectSelector, cleanBtnSelector) {
         this.target = document.querySelector(targetSelector);
         // this.currentMonth = this.setCurrentMonth();
-        this.categories = this.getCategories();
-        this.accounts = this.getAccounts();
         // this.accountSelect = document.querySelector(accountSelectSelector);
         // this.catSelect = document.querySelector(catSelectSelector);
         // this.cleanBtn = document.querySelector(cleanBtnSelector);
+    }
 
+    // setCurrentMonth(){
+    //     let currentMonth = ((new Date()).getMonth() + 1).toString();
+
+    //     if (currentMonth.length === 1){
+    //         return "0" + currentMonth;
+    //     } else {
+    //         return currentMonth;
+    //     }
+    // }
+
+    async getCategories(){
+        let response = await fetch(`/app/data/get_data.php?d=categories`);
+        const categories = await response.json(); 
+        return await categories
+    }
+
+    async getAccounts(){
+        let response = await fetch(`/app/data/get_data.php?d=accounts`);
+        return await response.json();
+    }
+
+    async mainChart() {
+        const chartData = {
+            category: [],
+            balance: []
+        }
+        const categories = await this.getCategories();
+
+        categories.forEach(async (cat) => {
+            let response = await fetch(`/app/data/get_data.php?d=balances&category=${cat.nome_cat}&mainCat=true&month=current`)
+            const data = await response.json();
+
+            data.forEach((d) => {
+                chartData.category.push(d.category);
+                chartData.balance.push(-d.balance);
+            })
+        })
+
+        this.renderChart(chartData);
+    }
+
+    renderChart(chartData) {
         var options = {
-            series: [44, 55, 41, 17, 15],
+            series: chartData.balance,
+            labels: chartData.category,
             chart: {
             type: 'donut',
             height: 300,
+          },
+          noData: {
+            text: 'Carregando...'
           },
           responsive: [{
             breakpoint: 480,
@@ -31,62 +76,7 @@ export default class MonthlyChart {
 
         this.chart = new ApexCharts(this.target, options);
         this.chart.render();
-        this.mainChart()
-    }
-
-    // setCurrentMonth(){
-    //     let currentMonth = ((new Date()).getMonth() + 1).toString();
-
-    //     if (currentMonth.length === 1){
-    //         return "0" + currentMonth;
-    //     } else {
-    //         return currentMonth;
-    //     }
-    // }
-
-    async getCategories(){
-        let response = await fetch(`/app/data/get_data.php?d=categories`);
-        return await response.json();
-    }
-
-    async getAccounts(){
-        let response = await fetch(`/app/data/get_data.php?d=accounts`);
-        return await response.json();
-    }
-
-    async mainChart() {
-        // categories["categories"].forEach(async (cat) => {
-        //     let response = await fetch(`/app/data/get_data.php?d=balances&category=${cat.nome_cat}&mainCat=true&month=current`)
-        //     const data = await response.json();
-
-        //     console.log(data)
-
-        // })
-
-        // let data = await fetch(`/app/data/get_data.php?d=balances`);
-        // data = await data.json();
-
-        // const chartData = {
-        //     months: [],
-        //     incomes: [],
-        //     expenses: []
-        // }
-        // for (let i = 1; i <= 12; i++) {
-        //     chartData.months.push(i);
-        //     chartData.incomes.push(data[i].incomes);
-        //     chartData.expenses.push(-data[i].expenses);
-        // }
-
-        // this.updateChart();
-    }
-
-    // updateChart() {
-    //     this.chart.updateSeries([{
-    //         name: 'Receitas',
-    //         data: [5, 20, 35, 40],
-    //         color: '#3e7f26'
-    //     }])
-    // }
+        }
 
     init() {
         // this.addOptionEvents = this.addOptionEvents.bind(this);
